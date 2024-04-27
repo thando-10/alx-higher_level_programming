95594b657c0841c5c264b96cf911a11ded165683
thando-10 <thandoxsndlovu@gmail.com>
2024-04-27 03:35:27 -0700

    first comment

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print("- Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+except urllib.error.URLError as e:
+    print("Error fetching URL:", e)
+
